f6846dfda066c1cc0e6da206007347d98797630a
Gautam Korlam <kageiit@gmail.com>
2020-12-30 10:35:18 -0800

    Cleanup debug statements

--- app/main.py
+++ app/main.py
@@ -11,16 +11,10 @@ app = FastAPI()
 configs=None
 if os.path.isdir(CONFIG_DIRECTORY):
     configs = os.listdir(CONFIG_DIRECTORY)
-print(configs)
 
 interface=None
 if os.path.isdir(INTERFACE_DIRECTORY):
     interface=os.path.join(INTERFACE_DIRECTORY, "wg0.conf")
-print(interface)
-
-@app.get("/")
-def root():
-    return "Nothing to see here!"
 
 @app.get("/randomize")
 def randomize():
